a67c8d3805613a741e3d7e0e1d97bba527eaacb7
jamp-mazon <joseantoniomazonperez@gmail.com>
2025-10-02 13:12:13 +0200

    intento de insertar fichero en ej casa

--- casa/1-oct-2025/formulario.php
+++ casa/1-oct-2025/formulario.php
@@ -11,22 +11,41 @@ if ($_SERVER["REQUEST_METHOD"]=="POST") {
         $apellidos=trim(htmlspecialchars(strip_tags($_POST["apellidos"])));
     }
     else{
-        $edadError="formato de apellidos incorrecto, pruebe de otra forma";
+        $apellidosError="formato de apellidos incorrecto, pruebe de otra forma";
     }
     if (isset($_POST["email"]) && filter_var($email,FILTER_VALIDATE_EMAIL)) {
         $email=trim(htmlspecialchars(strip_tags($_POST["email"])));
 
-    }
-     
+    } 
     else {
         $emailError="Email incorrecto";
     }
+    $permitidos=["Musica","Cine","Lectura"];
+    if (isset($_POST["aficiones"])) {
+        $seleccionados=array_intersect($_POST["aficiones"],$permitidos);//array_intersect compara el contenido de un array con el contenido de otro array.
+        if ($seleccionados) {
+            $aficiones=$seleccionados;
+        }
+        else{
+            $errorCheckBox="Campos incorrectos...";
+        }
+    }
+  if ($_FILES["fichero"]["size"]>1000000) {
+    $mensaje="Archivo demasiado grande";
+  }
+  else{
+    //Tamañao adecuado del archivo
+    $ruta_subida="bbdd/";
+    $res= move_uploaded_file($_FILES["fichero"]["tmp_name"],$ruta_subida . $_FILES["fichero"]["name"]);
+    if ($res) {
+      $mensaje="Fichero guardado correctamente";
+    }
+    else{
+      $mensaje="Error al guardar el fichero";
+    }
+  }    
     
-} else {
-    # code...
 }
-
-
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -73,7 +92,26 @@ if ($_SERVER["REQUEST_METHOD"]=="POST") {
                         <input type="checkbox" name="aficiones[]" value="Cine">Cine</input><br>
                         <input class="ml-6"type="checkbox" name="aficiones[]" value="Lectura">Lectura</input><br>
                     </div>
+                    <div>
+                        <input type="file"name="archivo">
+                    </div>
                     <button class="mt-2 bg-blue-600 p-1 rounded text-white cursor-pointer hover:bg-blue-900" type="submit">Enviar</button>
+                    
+                    <?php
+                    if(isset($nombreError)){
+                        echo "<p class='text-red-700'>ERROR:Nombre</p>";
+                    }
+                    if(isset($apellidosError)){
+                        echo "<p class='text-red-700'>ERROR:$apellidosError</p>";
+                    }
+                    if(isset($emailError)){
+                        echo "<p class='text-red-700'>ERROR:$emailError</p>";
+                    }
+                    if(isset($errorCheckBox)){
+                        echo "<p class='text-red-700'>ERROR:$errorCheckBox</p>";
+                    } 
+                                                                                 
+                    ?>
                 </div>
             </form>
         </article>
