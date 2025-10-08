0cb52377adea490898e3ef505f44df3e0375a45c
Jose Antonio Mazón <joseantoniomazonperez@gmail.com>
2025-10-08 17:34:59 +0200

    continuacion de ej6

--- tema2_php/2_formularios/form06_libros/procesar_alta.php
+++ tema2_php/2_formularios/form06_libros/procesar_alta.php
@@ -4,6 +4,7 @@ if ($_SERVER["REQUEST_METHOD"]!="POST") {
 } else {
     $tituloOK=false;$autorOK=false;$anioOk=false;$generosOK=false;$caratulaOK=false;
     $mensajeError="";
+    $generos=[];//me creo un array vacio porque los generos seran en formato array.
     //Comprobaciones de las cosas
     if (isset($_POST["titulo"])&& $_POST["titulo"]!="") {
         $titulo=trim(htmlspecialchars(strip_tags($_POST["titulo"])));
@@ -11,6 +12,8 @@ if ($_SERVER["REQUEST_METHOD"]!="POST") {
     }
     else{
         $mensajeError="ERROR:en el titulo...";
+        header("Location:alta.php");
+        die;
     }
     if (isset($_POST["autor"])&& $_POST["autor"]!="") {
         $autor=trim(htmlspecialchars(strip_tags($_POST["autor"])));
@@ -18,12 +21,26 @@ if ($_SERVER["REQUEST_METHOD"]!="POST") {
     }
     else{
         $mensajeError="ERROR: el autor es incorrecto";
+        header("Location:alta.php");
+        die;
+
     }
     if (isset($_POST["anio"]) && $_POST["anio"]!="") {
         $anio=trim(htmlspecialchars(is_numeric($_POST["anio"])));
         $anioOk=true;
     } else {
         $mensajeError="ERROR: año incorrecto";
+        header("Location:alta.php");
+        die;
+    }
+    if (isset($_POST["generos"])&& $_POST["generos"]!=[]) {
+        $generos=trim(htmlspecialchars(is_array($_POST["generos"])));
+        $generos=true;
+    }
+    else{
+        $mensajeError="ERROR:los generos no pueden estar vacios";
+        header("Location:alta.php");
+        die;
     }
     
 }
